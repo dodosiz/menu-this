ff683bc5cfc5178a284ed918f84bc77302fbe288
Theo Asvestopoulos <t.v.asvestopoulos@gmail.com>
2023-02-19 01:47:34 +0100

    Fixed the index mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,15 +20,15 @@ export default function Home() {
     <Layout user={user}>
       <>
         <div className={styles.banner}>
-          <Heading size="2xl" className={styles.banner_header} as="h1">
+          <Heading size="xl" className={styles.banner_header} as="h1">
             Create your{" "}
             <span className={styles.banner_header_coloured}>online menu</span>
           </Heading>
           <Text className={styles.banner_text} fontSize="md">
-            Use our tool to create the menu of your business online. Create
-            categories and add products with prices, this is all you need to do.
-            Everything else, like the URL and the QR code for the menu is done
-            by us.
+            Use our tool to create the menu of your business online.
+            <br /> Create categories and add products, this is all you need to
+            do.
+            <br /> Everything else is done by us.
           </Text>
           <Button
             className={styles.banner_button}
