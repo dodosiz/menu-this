329c7f0cdd40a0855bf45936fb157cee272d22d7
Theo Asvestopoulos <t.v.asvestopoulos@gmail.com>
2023-02-28 00:37:41 +0100

    Menu QR code

---
+++ src/components/qrModalDialog.tsx
@@ -0,0 +1,65 @@
+import { SECONDARY_COLOR } from "@/styles/constants";
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+import { useQRCode } from "next-qrcode";
+
+interface QrDialogProps {
+  url: string;
+  isOpen: boolean;
+  setOpen: (b: boolean) => void;
+}
+
+export function QrDialog({ url, isOpen, setOpen }: QrDialogProps) {
+  const { Canvas } = useQRCode();
+  return (
+    <Modal isOpen={isOpen} onClose={() => setOpen(false)}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Menu QR</ModalHeader>
+        <ModalCloseButton />
+
+        <ModalBody>
+          <Text color={SECONDARY_COLOR}>
+            Use this QR code to let customers scan your menu.
+          </Text>
+          <Box>
+            <Canvas
+              text={url}
+              options={{
+                level: "M",
+                margin: 3,
+                scale: 4,
+                width: 200,
+                color: {
+                  dark: "#000",
+                  light: "#fff",
+                },
+              }}
+            />
+          </Box>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            variant="outline"
+            colorScheme="teal"
+            mr={3}
+            onClick={() => setOpen(false)}
+          >
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
--- src/components/viewMenuButtons.tsx
+++ src/components/viewMenuButtons.tsx
@@ -1,7 +1,9 @@
 import { Button, IconButton } from "@chakra-ui/react";
-import { ImEye } from "react-icons/im";
+import { useState } from "react";
+import { ImEye, ImQrcode } from "react-icons/im";
 import styles from "@/styles/components/viewMenuButtons.module.css";
 import { useRouter } from "next/router";
+import { QrDialog } from "./qrModalDialog";
 
 interface ViewMenuButtonsProps {
   menuId: string;
@@ -9,8 +11,14 @@ interface ViewMenuButtonsProps {
 
 export function ViewMenuButtons({ menuId }: ViewMenuButtonsProps) {
   const router = useRouter();
+  const [showQR, setShowQR] = useState(false);
   return (
     <>
+      <QrDialog
+        isOpen={showQR}
+        setOpen={setShowQR}
+        url={`${window.location.origin}/menu/${menuId}`}
+      />
       <Button
         leftIcon={<ImEye />}
         colorScheme="teal"
@@ -28,6 +36,24 @@ export function ViewMenuButtons({ menuId }: ViewMenuButtonsProps) {
         size="sm"
         onClick={() => router.push(`/menu/${menuId}`)}
         className={`${styles.view_button} ${styles.view_button_mobile}`}
+      />
+      <Button
+        leftIcon={<ImQrcode />}
+        colorScheme="teal"
+        variant="outline"
+        onClick={() => setShowQR(true)}
+        className={`${styles.qr_button} ${styles.qr_button_desktop}`}
+      >
+        View
+      </Button>
+      <IconButton
+        icon={<ImQrcode />}
+        colorScheme="teal"
+        aria-label="qr code"
+        variant="outline"
+        size="sm"
+        onClick={() => setShowQR(true)}
+        className={`${styles.qr_button} ${styles.qr_button_mobile}`}
       />
     </>
   );
