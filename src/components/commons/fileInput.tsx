d5fd36cf174a53087abce76fc2cbb3356dd2eaa0
Theo Asvestopoulos <t.v.asvestopoulos@gmail.com>
2023-04-27 01:25:36 +0200

    Landscape photo hint

--- src/components/commons/fileInput.tsx
+++ src/components/commons/fileInput.tsx
@@ -13,59 +13,64 @@ interface FileInputProps {
 
 export default function FileInput({ onChange }: FileInputProps) {
   return (
-    <AspectRatio width="40" ratio={1}>
-      <Box
-        borderColor="gray.300"
-        borderStyle="dashed"
-        borderWidth="2px"
-        rounded="md"
-        shadow="sm"
-        role="group"
-        transition="all 150ms ease-in-out"
-        _hover={{
-          shadow: "md",
-        }}
-      >
-        <Box position="relative" height="100%" width="100%">
-          <Box
-            position="absolute"
-            top="0"
-            left="0"
-            height="100%"
-            width="100%"
-            display="flex"
-            flexDirection="column"
-          >
-            <Stack
+    <>
+      <AspectRatio width="40" ratio={1}>
+        <Box
+          borderColor="gray.300"
+          borderStyle="dashed"
+          borderWidth="2px"
+          rounded="md"
+          shadow="sm"
+          role="group"
+          transition="all 150ms ease-in-out"
+          _hover={{
+            shadow: "md",
+          }}
+        >
+          <Box position="relative" height="100%" width="100%">
+            <Box
+              position="absolute"
+              top="0"
+              left="0"
               height="100%"
               width="100%"
               display="flex"
-              alignItems="center"
-              justify="center"
-              spacing="4"
+              flexDirection="column"
             >
-              <Stack p="8" textAlign="center" spacing="1">
-                <Heading fontSize="lg" color="gray.700" fontWeight="bold">
-                  Drop images here
-                </Heading>
-                <Text fontWeight="light">or click to upload</Text>
+              <Stack
+                height="100%"
+                width="100%"
+                display="flex"
+                alignItems="center"
+                justify="center"
+                spacing="4"
+              >
+                <Stack p="8" textAlign="center" spacing="1">
+                  <Heading fontSize="lg" color="gray.700" fontWeight="bold">
+                    Drop images here
+                  </Heading>
+                  <Text fontWeight="light">or click to upload</Text>
+                </Stack>
               </Stack>
-            </Stack>
+            </Box>
+            <Input
+              type="file"
+              height="100%"
+              width="100%"
+              position="absolute"
+              top="0"
+              left="0"
+              opacity="0"
+              aria-hidden="true"
+              accept="image/*"
+              onChange={onChange}
+            />
           </Box>
-          <Input
-            type="file"
-            height="100%"
-            width="100%"
-            position="absolute"
-            top="0"
-            left="0"
-            opacity="0"
-            aria-hidden="true"
-            accept="image/*"
-            onChange={onChange}
-          />
         </Box>
-      </Box>
-    </AspectRatio>
+      </AspectRatio>
+      <Text fontWeight="light">
+        * Use landscape photos for better layout design.
+      </Text>
+    </>
   );
 }
